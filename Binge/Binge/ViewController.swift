7e5e5fa8d66089f05123fd928ece6a168b549a41
Author <m.gravbrot@gmail.com>
2017-06-09 14:35:09 +0200

    Project initialization

---
+++ Binge/Binge/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Binge
+//
+//  Created by Marco Gravbrøt on 09/06/2017.
+//  Copyright © 2017 Marco Gravbrøt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
